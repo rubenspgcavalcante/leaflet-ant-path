9abd9a4b13784f9a107417abd8ace943c3a50fe8
Rubens Pinheiro <rubenspgcavalcante@gmail.com>
2016-01-20 10:49:22 -0300

    Demo of version 0.0.1

--- example.js
+++ example.js
@@ -1,11 +1,17 @@
-(function () {
-    var map = L.map('map').setView([-3.7313665, -38.47171], 13);
-    var line = [[-3.7313665, -38.47171, 15], [-3.7340995, -38.4724879], [-3.7483599, -38.4605145]];
+$(document).ready(function(){
+    var map = L.map('map').setView([0, 0], 3);
 
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    $.getJSON('route.json', function(route){
+        var line = route;
 
-    var layer = new L.AntPath(line);
-    map.addLayer(layer);
-}());
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        var layer = new L.AntPath(line);
+        map.addLayer(layer);
+        map.fitBounds(layer.getBounds());
+
+    });
+
+});
