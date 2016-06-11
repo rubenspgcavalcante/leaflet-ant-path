b4f59daeb9ff0c94a826c02170ccdafb594c4383
Rubens Pinheiro <rubenspgcavalcante@gmail.com>
2016-06-11 10:02:20 -0300

    Fix on running karma using gulp

--- tasks/test.task.js
+++ tasks/test.task.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var karma = require('karma');
 
-gulp.task('test', function (done) {
+gulp.task('test', function () {
     return new karma.Server({
         configFile: __dirname + '/../karma.conf.js',
         singleRun: true
-    }, done).start();
+    }, function(){
+        process.exit();
+    }).start();
 });
