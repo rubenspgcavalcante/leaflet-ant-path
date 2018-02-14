4774291b72f53a573b0ed1559a19775d28284c42
Rubens Pinheiro <rubenspgcavalcante@gmail.com>
2018-02-14 14:59:42 +0100

    Fixes on examples

--- app/modules/docs/components/stateless/Using.jsx
+++ app/modules/docs/components/stateless/Using.jsx
@@ -9,22 +9,22 @@ const table = {
   rows: [
     ["pause()", "boolean", "Stops the animation"],
     ["resume()", "boolean", "Resume the animation"],
-    ["reverse()", "boolean", "Revert the animation"],
+    ["reverse()", <span><b>this</b> instance</span>, "Revert the animation"],
     ["map(callback)", "new AntPath or extended class", "Iterates over the latlngs"]
   ]
 };
 
 export default ({ title, id }) => (
-  <DocSection id={id} title={title} >
-    <p >The Ant Path layer, both constructor and factory, can receive the same options
-      of a common <a href="http://leafletjs.com/reference-1.2.0.html#polyline" target="blank" >Polyline <span
-        className="icon is-small" ><i className="fa fa-external-link" /></span > </a >, plus the
-      following options:</p >
-    <div className="highlight" >
-      <CodeSnippet id="withOptions" />
-      <Info >The above options are the default ones</Info >
-    </div >
-    <p ><b >Methods</b ></p >
+  <DocSection id={id} title={title}>
+    <p>The Ant Path layer, both constructor and factory, can receive the same options
+      of a common <a href="http://leafletjs.com/reference-1.2.0.html#polyline" target="blank">Polyline <span
+        className="icon is-small"><i className="fa fa-external-link"/></span> </a>, plus the
+      following options:</p>
+    <div className="highlight">
+      <CodeSnippet id="withOptions"/>
+      <Info>The above options are the default ones</Info>
+    </div>
+    <p><b>Methods</b></p>
     <Table {...table}/>
-  </DocSection >
+  </DocSection>
 );
--- app/modules/examples/components/Examples.jsx
+++ app/modules/examples/components/Examples.jsx
@@ -5,16 +5,18 @@ import CodePen from 'modules/ui/components/CodePen';
 export default class Examples extends PureComponent {
   render() {
     return (
-      <div className="examples" >
-        <h1 className="title" >Examples</h1 >
-        <h2 className="subtitle" >Multiple paths</h2 >
-        <FasterOption />
-        <hr />
-        <h1 className="title" >Try by yourself</h1 >
-        <h2 className="subtitle" >Editable examples</h2 >
+      <div className="examples">
+        <h1 className="title">Examples</h1>
+        <h2 className="subtitle">Multiple paths</h2>
+        <FasterOption/>
+        <hr/>
+        <h1 className="title">Try by yourself</h1>
+        <h2 className="subtitle">Editable examples</h2>
+        <CodePen title="Stopping and Resuming Route" hash="BYdgXR"
+                 description="You can stop and resume the animation any time"/>
         <CodePen title="Reverse Route" hash="MvdJbb"
-                 description="Here you can see a simple manner to reverse the route flux" />
-      </div >
+                 description="You can reverse the route flux"/>
+      </div>
     );
   }
 };
