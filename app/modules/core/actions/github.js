e099559b70f4ec37481db585e7d5faf76aaeaa7c
Rubens Pinheiro Gonçalves Cavalcante <rubenspgcavalcante@gmail.com>
2018-12-20 11:15:05 +0100

    Fixed epics

--- app/modules/core/actions/github.js
+++ app/modules/core/actions/github.js
@@ -1,17 +1,26 @@
-import { filter, map, mergeMap } from "rxjs/operators";
+import { of } from "rxjs";
+import { filter, map, mergeMap, catchError } from "rxjs/operators";
 import { ajax } from "rxjs/ajax";
-import { LOAD_REPO_INFO, REPO_INFO_LOADED } from "./index";
+import { LOAD_REPO_INFO, REPO_INFO_LOADED, REPO_INFO_ERROR } from "./index";
 
 const ghApi = action => `https://api.github.com${action}`;
 export const loadRepoInfo = () => ({ type: LOAD_REPO_INFO });
+
 export const repoInfoLoaded = info => ({
   type: REPO_INFO_LOADED,
   payload: info
+});
+
+export const repoInfoError = err => ({
+  type: REPO_INFO_ERROR,
+  payload: err
 });
 
 export const loadRepoInfoEpic = action$ =>
   action$.pipe(
     filter(ac => ac.type === LOAD_REPO_INFO),
     map(action => ghApi`/repos/rubenspgcavalcante/leaflet-ant-path`),
-    mergeMap(url => ajax.getJSON(url).pipe(map(res => repoInfoLoaded(res))))
+    mergeMap(url => ajax.getJSON(url)),
+    map(res => repoInfoLoaded(res)),
+    catchError(err => of(repoInfoError(err)))
   );
--- app/modules/core/actions/index.js
+++ app/modules/core/actions/index.js
@@ -1,9 +1,10 @@
-export const LOAD_REPO_INFO = 'LOAD_REPO_INFO';
-export const REPO_INFO_LOADED = 'REPO_INFO_LOADED';
+export const LOAD_REPO_INFO = "LOAD_REPO_INFO";
+export const REPO_INFO_LOADED = "REPO_INFO_LOADED";
+export const REPO_INFO_ERROR = "REPO_INFO_ERROR";
 
-export const APP_ROUTES = 'APP_ROUTES';
+export const APP_ROUTES = "APP_ROUTES";
 
-export const LOAD_ROUTE = 'LOAD_ROUTE';
-export const ROUTE_LOADED = 'ROUTE_LOADED';
+export const LOAD_ROUTE = "LOAD_ROUTE";
+export const ROUTE_LOADED = "ROUTE_LOADED";
 
-export const LOADING = 'LOADING';
+export const LOADING = "LOADING";
