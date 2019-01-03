323bb2037ec0382c0e2307549cd3d9bd1850e531
Rubens Pinheiro Gonçalves Cavalcante <rubenspgcavalcante@gmail.com>
2019-01-03 13:03:09 +0100

    Fixes on main snippet

--- app/utils/code-snippets/main-demo.js
+++ app/utils/code-snippets/main-demo.js
@@ -1,53 +1,56 @@
-import { POLYLINE } from "../vectors.constant";
-const bracklessJSON = obj => JSON.stringify(obj).replace(/\{|\}/, "");
+import { POLYLINE, CURVE } from "../vectors.constant";
+const bracklessJSON = obj =>
+  JSON.stringify(obj, null, "  ").replace(/\{|\}/, "");
 
 const isPoly = vector => vector === POLYLINE;
-const useLVector = use => (isPoly(use) ? "" : `"use": L.${use}, `);
-const useVector = use => (isPoly(use) ? "" : `"use": ${use}, `);
-const includeVector = use => (isPoly(use) ? "" : ` ${use},`);
+const isCurve = vector => vector === CURVE;
 
-export const es5 = ({ use, ...rest }) => `
-const path = L.polyline.antPath(route,
-  { ${useLVector(use) + bracklessJSON(rest)} }
-);
+const useLVector = (use, always=false) =>
+  isPoly(use) ? "" : `\n  "use": ${isCurve(use) || always? "L." : ""}${use}, `;
+
+const includeVector = use => (isPoly(use) || isCurve(use) ? "" : `${use}, `);
+
+export const es5 = ({ use, ...rest }) => `const path = L.polyline.antPath(route, {${useLVector(use, true) +
+  bracklessJSON(rest)});
 const myMap = L.map('map').setView([0, 0], 13);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-  attribution: "&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors"
+  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 myMap.addLayer(path);
 myMap.fitBounds(path.getBounds())`;
 
-export const es6 = ({ use, ...rest }) => `
-import {${includeVector(use)} map, tileLayer} from 'leaflet';
+export const es6 = ({ use, ...rest }) => `import ${isCurve(use) ? "L," : ""}{${includeVector(
+  use
+)}map, tileLayer} from 'leaflet';${
+  isCurve(use) ? `\nimport '@elfalem/leaflet-curve';` : ""
+}
 import {antPath} from 'leaflet-ant-path';
 import latLngs from 'sample-${use}.json'; //This is a example, the JSON can come from any place
 
-const path = antPath(latLngs,
-  { ${useVector(use) + bracklessJSON(rest)} }
-);
+const path = antPath(route, {${useLVector(use) + bracklessJSON(rest)});
 const myMap = map('map').setView([0, 0], 13);
 
 tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-  attribution: "&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors"
+  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 myMap.addLayer(path);
 myMap.fitBounds(path.getBounds())`;
 
-export const browserify = ({ use, ...rest }) => `
-const {${includeVector(use)} map, tileLayer} = require('leaflet');
+export const browserify = ({ use, ...rest }) => `const L = require('leaflet').default;${
+  isCurve(use) ? `\nrequire('@elfalem/leaflet-curve');` : ""
+}
 const {antPath} = require('leaflet-ant-path');
 const route = require('sample-route.json'); //This is a example, the JSON can come from any place
 
-const path = antPath(route,
-  { ${useVector(use) + bracklessJSON(rest)} }
-);
+const {${includeVector(use)}map, tileLayer} = L;
+const path = antPath(route, {${useLVector(use) + bracklessJSON(rest)});
 const myMap = map('map').setView([0, 0], 13);
 
 tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-  attribution: "&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors"
+  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 myMap.addLayer(path);
